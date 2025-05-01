6b2d115904047e25a7d33e64161d27bcba5c09d6
quanturtle <alvaromachucar@gmail.com>
2025-05-01 09:18:03 -0300

    working casarica

---
+++ pipeline/dags/scrape_products_casarica.py
@@ -0,0 +1,183 @@
+'''
+DAG: scrape_products_casarica
+PRODUCTS_HTML --> PRODUCTS
+'''
+import json
+import broker
+from datetime import datetime
+from requests.exceptions import RequestException
+from redis import RedisError
+from redis.exceptions import ResponseError
+from airflow.decorators import dag, task
+from airflow.exceptions import AirflowNotFoundException
+from airflow.providers.redis.hooks.redis import RedisHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from bs4 import BeautifulSoup
+
+
+DEFAULT_ARGS = {
+    'owner': 'data-engineering',
+    'retries': 0,
+}
+
+POSTGRES_CONN_ID = 'my-db'
+REDIS_CONN_ID = 'my-redis'
+OUTPUT_STREAM_NAME = 'products_stream'
+TRANSFORM_STREAM_NAME = 'transform_products_stream'
+GROUP_NAME = 'product_db_inserters'
+CONSUMER_NAME = 'transformer'
+
+PRODUCT_CONTAINER_TAG = 'div'
+PRODUCT_CONTAINER_CLASS = 'single-product-wrapper'
+
+PRODUCT_NAME_TAG = 'h1'
+PRODUCT_NAME_CLASS = 'product_title entry-title'
+
+PRODUCT_SKU_TAG = 'span'
+PRODUCT_SKU_CLASS = 'sku'
+PRODUCT_SKU_ATTRS = {'id': 'producto-codigo'}
+
+PRODUCT_PRICE_TAG = ['p', 'span']
+PRODUCT_PRICE_CLASS = ['price', 'ecommercepro-Price-amount amount']
+PRODUCT_PRICE_ATTRS = [{}, {'id': 'producto-precio'}]
+
+
+@dag(
+    default_args=DEFAULT_ARGS,
+    tags=['casarica', 'etl'],
+    catchup=False,
+)
+def scrape_products_casarica():
+    @task()
+    def setup_transform_stream():
+        my_broker = broker.Broker(redis_connection_id=REDIS_CONN_ID)
+        my_broker.create_connection()
+        
+        my_broker.create_xgroup(TRANSFORM_STREAM_NAME, GROUP_NAME)
+
+        return
+    
+
+    @task()
+    def extract_products_html():
+        hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+
+        sql = '''
+            SELECT supermarket_id, html, url
+            FROM products_html
+            WHERE supermarket_id = 5
+            LIMIT 1;
+        '''
+
+        results = hook.get_records(sql)
+
+        if not results:
+            raise AirflowNotFoundException('No product HTML content found for casarica in `products_html` table.')
+
+        my_broker = broker.Broker(redis_connection_id=REDIS_CONN_ID)
+        my_broker.create_connection()
+
+        products_htmls = []
+        
+        for row in results:
+            products_htmls.append({
+                'supermarket_id': row[0],
+                'html': row[1],
+                'url': row[2]
+            })
+
+        my_broker.write_pipeline(TRANSFORM_STREAM_NAME, *products_htmls)
+        
+        return
+
+
+    @task()
+    def transform_products_html_to_products():
+        my_broker = broker.Broker(redis_connection_id=REDIS_CONN_ID)
+        my_broker.create_connection()
+
+        while True:
+            batch = my_broker.read(TRANSFORM_STREAM_NAME, GROUP_NAME, CONSUMER_NAME, batch_size=20, block_time_ms=5_000)        
+        
+            if batch is None:
+                break
+
+            for product_html in batch:
+                soup = BeautifulSoup(product_html['html'], 'html.parser')
+                
+                product_details_container = soup.find(PRODUCT_CONTAINER_TAG, class_=PRODUCT_CONTAINER_CLASS)
+                
+                if not product_details_container:
+                    product = {
+                        'supermarket_id': product_html['supermarket_id'],
+                        'description': '',
+                        'sku': '',
+                        'price': '',
+                        'url': product_html['url'],
+                        'created_at': datetime.now().isoformat()
+                    }
+                    
+                    my_broker.ack(TRANSFORM_STREAM_NAME, GROUP_NAME, *[product_html['entry_id']])
+                    my_broker.write(OUTPUT_STREAM_NAME, product)
+                
+                    continue
+
+                try:        
+                    product_description = product_details_container.find(
+                        PRODUCT_NAME_TAG, class_=PRODUCT_NAME_CLASS
+                    ).text.strip().upper()
+                
+                except:
+                    product_description = product_html.get('description', '')
+
+                try:
+                    product_sku = product_details_container.find(
+                        PRODUCT_SKU_TAG, class_=PRODUCT_SKU_CLASS, attrs=PRODUCT_SKU_ATTRS
+                    ).text.strip()
+                    product_sku = ''.join(filter(str.isdigit, product_sku))
+                
+                except:
+                    product_sku = ''
+
+                try:
+                    product_price_container = product_details_container.find(
+                        PRODUCT_PRICE_TAG[0], class_=PRODUCT_PRICE_CLASS[0], attrs=PRODUCT_PRICE_ATTRS[0]
+                    )
+                    
+                    if product_price_container:
+                        product_price_span = product_price_container.find(
+                            PRODUCT_PRICE_TAG[1], class_=PRODUCT_PRICE_CLASS[1]
+                        ).text.strip()
+                        product_price = ''.join(filter(str.isdigit, product_price_span))
+                        product_price = int(product_price) if product_price else 0
+                    
+                    else:
+                        product_price = None
+                
+                except:
+                    product_price = None
+                
+                product = {
+                    'supermarket_id': product_html['supermarket_id'],
+                    'description': product_description,
+                    'sku': product_sku,
+                    'price': product_price,
+                    'url': product_html['url'],
+                    'created_at': datetime.now().isoformat()
+                }
+
+                # load
+                my_broker.ack(TRANSFORM_STREAM_NAME, GROUP_NAME, *[product_html['entry_id']])
+                my_broker.write(OUTPUT_STREAM_NAME, product)
+                
+        return
+
+
+    setup = setup_transform_stream()
+    extract = extract_products_html()    
+    transform = transform_products_html_to_products()
+
+    setup >> extract >> transform
+
+
+scrape_products_casarica()
---
+++ pipeline/dags/scrape_products_html_casarica.py
@@ -0,0 +1,122 @@
+'''
+DAG: scrape_products_html_casarica
+PRODUCT_URLS --> PRODUCTS_HTML
+'''
+import broker
+from datetime import datetime
+import requests
+from requests.exceptions import RequestException
+from redis import RedisError
+from airflow.decorators import dag, task
+from airflow.exceptions import AirflowNotFoundException
+from airflow.providers.redis.hooks.redis import RedisHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+import time
+
+
+DEFAULT_ARGS = {
+    'owner': 'data-engineering',
+    'retries': 0,
+}
+
+POSTGRES_CONN_ID = 'my-db'
+REDIS_CONN_ID = 'my-redis'
+OUTPUT_STREAM_NAME = 'products_html_stream'
+TRANSFORM_STREAM_NAME = 'transform_products_html_stream'
+GROUP_NAME = 'product_db_inserters'
+CONSUMER_NAME = 'transformer'
+
+
+@dag(
+    default_args=DEFAULT_ARGS,
+    tags=['casarica', 'etl'],
+    catchup=False,
+)
+def scrape_products_html_casarica():
+    @task()
+    def setup_transform_stream():
+        my_broker = broker.Broker(redis_connection_id=REDIS_CONN_ID)
+        my_broker.create_connection()
+        
+        my_broker.create_xgroup(TRANSFORM_STREAM_NAME, GROUP_NAME)
+
+        return
+    
+    
+    @task()
+    def extract_product_urls():
+        hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+
+        sql = '''
+            SELECT supermarket_id, url
+            FROM product_urls
+            WHERE supermarket_id = 5
+            LIMIT 1;
+        '''
+
+        results = hook.get_records(sql)
+
+        if not results:
+            raise AirflowNotFoundException('No product URLs found for casarica in `product_urls` table.')
+
+        my_broker = broker.Broker(redis_connection_id=REDIS_CONN_ID)
+        my_broker.create_connection()
+
+        product_urls = []
+        
+        for row in results:
+            product_urls.append({
+                'supermarket_id': row[0],
+                'url': row[1]
+            })
+
+        my_broker.write_pipeline(TRANSFORM_STREAM_NAME, *product_urls)
+
+        return
+
+
+    @task()
+    def transform_product_urls_to_products_html():
+        my_broker = broker.Broker(redis_connection_id=REDIS_CONN_ID)
+        my_broker.create_connection()
+
+        while True:
+            batch = my_broker.read(TRANSFORM_STREAM_NAME, GROUP_NAME, CONSUMER_NAME, batch_size=20, block_time_ms=5_000)        
+        
+            if batch is None:
+                break
+
+            for product_url in batch:
+                try:
+                    time.sleep(0.5)
+                    
+                    response = requests.get(product_url['url'], timeout=30)
+                    response.raise_for_status()
+                    html_content = response.text
+                    
+                    product_html = {
+                        'supermarket_id': product_url['supermarket_id'],
+                        'html': html_content,
+                        'url': product_url['url'],
+                        'created_at': datetime.now().isoformat()
+                    }
+                    
+                except RequestException as e:
+                    print(f'Failed to fetch URL {product_url['url']}: {e}')
+                    
+                    return {'error': str(e)}
+
+                my_broker.ack(TRANSFORM_STREAM_NAME, GROUP_NAME, *[product_url['entry_id']])
+                my_broker.write(OUTPUT_STREAM_NAME, product_html)
+        
+        return
+
+
+    setup = setup_transform_stream()
+    extract = extract_product_urls()
+    transform = transform_product_urls_to_products_html()
+
+    setup >> extract >> transform
+
+
+scrape_products_html_casarica()
